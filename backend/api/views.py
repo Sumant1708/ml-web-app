c26755d83371623b1121e6b3d267ae5a956a1b2a
Sumant1708 <sumantdas170803@gmail.com>
2026-01-04 22:35:32 +0530

    Connect React frontend with Django ML prediction API

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
-
-# api/views.py
+import os
 import joblib
+from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
 
-model = joblib.load("model.pkl")
+BASE_DIR = settings.BASE_DIR
 
+MODEL_PATH = os.path.join(BASE_DIR, 'api', 'model.pkl')
+model = joblib.load(MODEL_PATH)
+
+@csrf_exempt
 @api_view(['POST'])
 def predict(request):
     value = request.data.get("value")
     prediction = model.predict([[value]])
     return Response({"prediction": prediction[0]})
-
